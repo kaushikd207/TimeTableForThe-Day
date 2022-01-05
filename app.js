075a5ce7f30c4b3df9e2815024371dfafd2ffab8
kaushikd207 <kaushikd207@gmail.com>
2022-01-05 20:31:19 +0530

    added

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-const wrap= document.querySelector('.lineText li:nth-child(2).lineInner');
-console.log(wrap);
+  
+ const list=document.querySelector('#taskList ul');
+ list.addEventListener('click',function(e){
+     if(e.target.className =='btn'){
+         const li=e.target.parentElement;
+         list.removeChild(li);
+     }
+ })
+
+ //add task function
+ const addTask=document.forms['addTask'];
+ addTask.addEventListener('submit', function(e){
+
+
+     e.prevent
+     e.preventDefault();
+     const value = addTask.querySelector('input[type="text"]').value;
+     
+
+    //  create task 
+    const li =document.createElement('li');
+    const taskName = document.createElement('p');
+    const btn= document.createElement('button');
+
+
+    btn.textContent='done';
+    taskName.textContent=value;
+    li.classList.add('lineInner');
+    btn.classList.add('btn');
+
+    li.appendChild(taskName);
+    li.appendChild(btn);
+    list.appendChild(li);
+ })
